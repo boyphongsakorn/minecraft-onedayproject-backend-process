03260892eaae90e75a25964a4cb1cbe7815a7c88
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-23 20:48:42 +0700

    ok

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const { spawn } = require('child_process');
 const cron = require('node-cron');
 // const Rcon = require('minecraft-rcon-client');
 const Rcon = require('rcon-client').Rcon;
+const { MessageAttachment, EmbedBuilder, Client, GatewayIntentBits, ButtonBuilder, SelectMenuBuilder, ActionRowBuilder, ClientUser, AttachmentBuilder } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 const options = {
   host: '127.0.0.1',
@@ -26,6 +29,11 @@ const options = {
 // })
 
 const rcon = new Rcon(options);
+
+client.once('ready', () => {
+  client.user.setPresence({ activities: [{ name: 'One Day Project Discord Bot', type: 'PLAYING' }], status: 'online' });
+  console.log('Ready!');
+});
 
 //get this location 
 
@@ -68,7 +76,30 @@ cron.schedule('*/5 * * * *', () => {
     startdate = new Date()
     startminecraft = true
     startmc()
-	console.log('start Minecraft Server')
+	  console.log('start Minecraft Server')
+    const msg = new EmbedBuilder()
+      .setColor('#0099ff')
+      .setTitle('One Day Project')
+      //.setURL('https://www.glo.or.th/')
+      .setDescription('เซิฟเวอร์เปิดแล้ว')
+      //.setThumbnail('https://raw.githubusercontent.com/boyphongsakorn/pwisetthon-discord-bot/master/docs/glologo.png')
+      /*.addFields(
+        { name: 'รางวัลที่หนึ่ง', value: json[0][1] },
+        //{ name: '\u200B', value: '\u200B' },
+        { name: 'เลขหน้าสามตัว', value: json[1][1] + ' | ' + json[1][2], inline: true },
+        { name: 'เลขท้ายสามตัว', value: json[2][1] + ' | ' + json[2][2], inline: true },
+        { name: 'เลขท้ายสองตัว', value: json[3][1] },
+      )*/
+      //.setImage('https://img.gs/fhcphvsghs/full,quality=low/' + imgurl + date + month + year)
+      .setImage('attachment://lottery_' + date + '' + month + '' + year + '.png')
+      .setTimestamp()
+      //.setFooter('ข้อมูลจาก rapidapi.com/boyphongsakorn/api/thai-lottery1 \nบอทจัดทำโดย Phongsakorn Wisetthon \nให้ค่ากาแฟ buymeacoffee.com/boyphongsakorn');
+      .setFooter({ text: 'ข้อมูลจาก rapidapi.com/boyphongsakorn/api/thai-lottery1 \nบอทจัดทำโดย Phongsakorn Wisetthon \nให้ค่ากาแฟ buymeacoffee.com/boyphongsakorn' });
+    client.channels.cache.get('932671627152461936').send({ embeds: [msg]})
+      .then(async function (message) {
+        //log message id
+        console.log(message);
+      });
   }
   //if time from startdate is over 24 hours and 30 minutes run minecraft rcon command to stop server
   if (new Date() - startdate > 24*60*60*1000 + 30*60*1000) {
