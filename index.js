3d3cdba7bb2d9c9254d67397ca65fdb5b2109b6e
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-02-23 19:46:48 +0700

    ok

--- index.js
+++ index.js
@@ -10,13 +10,22 @@
 
 const { spawn } = require('child_process');
 const cron = require('node-cron');
-const Rcon = require('minecraft-rcon-protocol');
+// const Rcon = require('minecraft-rcon-client');
+const Rcon = require('rcon-client').Rcon;
 
-const client = new Rcon({ // all of those are required!
-  port: process.env.RCON_PORT,
-  host: process.env.RCON_HOST,
-  password: process.env.RCON_PASSWORD
-})
+const options = {
+  host: '127.0.0.1',
+  port: 25575, // Default RCON port is 25575
+  password: 'minecraft'
+};
+
+// const client = new Rcon({ // all of those are required!
+//   port: process.env.RCON_PORT,
+//   host: process.env.RCON_HOST,
+//   password: process.env.RCON_PASSWORD
+// })
+
+const rcon = new Rcon(options);
 
 //get this location 
 
@@ -38,7 +47,7 @@ let startminecraft = false
 // });
 
 function startmc() {
-  const sh = spawn('sh', ['startmc.sh'], {cwd: '../'});
+  const sh = spawn('sh', ['startmc.bat'], {cwd: '../'});
 
   sh.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
@@ -55,26 +64,41 @@ function startmc() {
 
 cron.schedule('*/5 * * * *', () => {
   //if startdate is not set or real time is 23:30:00 set startdate to new Date() and run startmc.sh
-  if (!startdate || new Date().getHours() == 23 && new Date().getMinutes() == 30 && new Date().getSeconds() == 0) {
+  if (!startdate && new Date().getHours() == 23 && new Date().getMinutes() == 30 && new Date().getSeconds() == 0) {
     startdate = new Date()
     startminecraft = true
     startmc()
+	console.log('start Minecraft Server')
   }
   //if time from startdate is over 24 hours and 30 minutes run minecraft rcon command to stop server
   if (new Date() - startdate > 24*60*60*1000 + 30*60*1000) {
-    //run rcon command to stop server
-    client.connect().then(() => {
-      client.send("stop").then((response) => {
-          console.log(response)
-          startminecraft = false
-          client.disconnect()
-      }).catch(err => {
-          console.log("An error occurred while sending the query!")
-      })
-    }).catch(err => {
-      console.log("Connection to server cannot be established!")
-    })
+    // run rcon command to stop server
+    // client.connect().then(() => {
+    //   client.send("stop").then((response) => {
+    //       console.log(response)
+    //       startminecraft = false
+    //       client.disconnect()
+    //   }).catch(err => {
+    //       console.log("An error occurred while sending the query!")
+    //   })
+    // }).catch(err => {
+    //   console.log("Connection to server cannot be established!")
+    // })
+    rcon.connect().then(() => {
+      console.log('Connected to RCON');
+      rcon.send('stop').then(response => {
+        console.log(`Response: ${response}`);
+        rcon.end();
+      }).catch(error => {
+        console.error(error);
+        rcon.end();
+      });
+    }).catch(error => {
+      console.error(error);
+    });
     //if server is stopped run startmc.sh
     //if server is started set startdate to new Date()
   }
 })
+
+console.log('start now')
